f6e599adcdce43615cf2df7132e187599e8190f3
Rey David <cuevas.vela@gmail.com>
2020-01-20 12:39:06 -0600

    Agreagada la clase tablero

--- bin/main.rb
+++ bin/main.rb
@@ -2,16 +2,6 @@
 $tablero = [1,2,3,4,5,6,7,8,9]
 $turnos = 9
 
-def reglas
-  puts "Este es el juego del Gato
-      Es para dos jugadores
-      Las instrucciones para este juego son las siguientes
-      1.- El tablero se compone de 9 casillas
-      2.- El primer jugador juega con X y el segundo con O
-      3.- El primer jugador selecciona en que casilla poner su moviento despues el segundo y asi sucesivamente
-      4.- Gana quien logre tres casillas con su símbolo
-      5.- El juego termina cuando todos los tiros fueron hechos o se selecciona la opcion 0"
-end
 
 def muestra_tablero
   puts "╔═══╦═══╦═══╗"
---
+++ lib/tablero.rb
@@ -0,0 +1,30 @@
+class Tablero
+  attr_reader :tablero
+  attr_reader :lineas_ganadoras
+
+  def initialize
+    @tablero = [1,2,3,4,5,6,7,8,9]
+    @lineas_ganadoras = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  end
+  
+  def reglas
+    puts "Este es el juego del Gato
+      Es para dos jugadores
+      Las instrucciones para este juego son las siguientes
+      1.- El tablero se compone de 9 casillas
+      2.- El primer jugador juega con X y el segundo con O
+      3.- El primer jugador selecciona en que casilla poner su moviento despues el segundo y asi sucesivamente
+      4.- Gana quien logre tres casillas con su símbolo
+      5.- El juego termina cuando todos los tiros fueron hechos o se selecciona la opcion 0"
+  end
+
+  def dibuja_tablero
+    puts "╔═══╦═══╦═══╗"
+    puts "║ #{@@tablero[0]} ║ #{@@tablero[1]} ║ #{@@tablero[2]} ║"
+    puts "╠═══╬═══╬═══╣"
+    puts "║ #{@@tablero[3]} ║ #{@@tablero[4]} ║ #{@@tablero[5]} ║"
+    puts "╠═══╬═══╬═══╣"
+    puts "║ #{@@tablero[6]} ║ #{@@tablero[7]} ║ #{@@tablero[8]} ║"
+    puts "╚═══╩═══╩═══╝"
+  end
+end
