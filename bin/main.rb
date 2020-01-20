72e6319517b94cec8f9a84d437dcf999eb052f7c
Rey David <cuevas.vela@gmail.com>
2020-01-20 15:40:32 -0600

    linter fix

--- bin/main.rb
+++ bin/main.rb
@@ -1,24 +1,29 @@
 #!/usr/bin/env ruby
 
 require '../lib/tablero.rb'
+
 require'../lib/jugador.rb'
 
 def juego
   mi_tablero = Tablero.new
   mi_tablero.reglas
-  puts "Jugador #1 Introduce tu nombre"
-  jugador_x = Jugador.new(gets.chop.capitalize,"X")
-  puts "Jugador #2 Introduce tu nombre"
-  jugador_o = Jugador.new(gets.chop.capitalize, "O")
+  puts 'Jugador #1 Introduce tu nombre'
+  jugador_x = Jugador.new(gets.chop.capitalize, 'X')
+  puts 'Jugador #2 Introduce tu nombre'
+  jugador_o = Jugador.new(gets.chop.capitalize, 'O')
   mi_tablero.limpiar
   puts "Hola #{jugador_x.nombre} y #{jugador_o.nombre}, ¡Vamos a empezar!"
   mi_tablero.dibuja_tablero
   turno = true
   while true
-    puts turno ? "#{jugador_x.nombre} \"#{jugador_x.simbolo}\" Introduce tu tiro (1 - 9)" : "#{jugador_o.nombre} \"#{jugador_o.simbolo}\" Introduce tu tiro (1 - 9)"
+    if turno
+      puts "#{jugador_x.nombre} \"#{jugador_x.simbolo}\" Introduce tu tiro (1 - 9)"
+    else
+      puts "#{jugador_o.nombre} \"#{jugador_o.simbolo}\" Introduce tu tiro (1 - 9)"
+    end
     tiro = gets.chop
     case tiro
-    when "0"
+    when '0'
       break
     else
       break unless hay_juego = mi_tablero.tiro(tiro,turno)
@@ -35,6 +40,6 @@ end
 
 begin 
   juego
-  puts "¿Jugar de nuevo? S/N"
+  puts '¿Jugar de nuevo? S/N'
   op = gets.chop.downcase
-end while (op == "s") || (op == "y")
+end while (op == 's') || (op == 'y')
